63c6686791de97f36a16fe2c465185e585aa2d0a
cbkmutlu <cbkmutlu@gmail.com>
2025-05-01 01:14:04 +0300

    add migration defaults

--- Migration/Migration.php
+++ Migration/Migration.php
@@ -14,6 +14,7 @@ class Migration {
 
    protected function defaults(): string {
       return "
+         `is_deleted` BOOLEAN NOT NULL DEFAULT 0,
          `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
          `created_by` INT NOT NULL DEFAULT 0,
          `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
