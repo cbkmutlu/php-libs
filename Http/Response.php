3f9d585aaa946ca62443a14ef2cdc666bf6cdcde
cbkmutlu <cbkmutlu@gmail.com>
2025-05-01 01:15:11 +0300

    response methods

--- Http/Response.php
+++ Http/Response.php
@@ -26,36 +26,36 @@ class Response {
       return $this->codes[$code];
    }
 
-   public function success(string $message, mixed $data = null, int $code = 200): void {
+   public function success(mixed $data = null, ?string $message = null, int $code = 200): void {
       $this->json($message, $data, null, $code);
    }
 
-   public function error(string $message, mixed $data = null, int $code = 417): void {
-      $this->json($message, $data, null, $code);
+   public function error(?string $message = null, mixed $error = null, int $code = 400): void {
+      $this->json($message, null, $error, $code);
    }
 
-   public function json(string $message, mixed $data = null, mixed $errors = null, int $code = 200, array $meta = []): void {
+   public function json(?string $message = null, mixed $data = null, mixed $error = null, int $code = 200, ?array $meta = null): void {
       header_remove();
       $this->status($code);
 
-      header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+      if (ENV === 'production') {
+         if ($code >= 200 && $code < 300) {
+            header('Cache-Control: no-transform, public, max-age=300, s-maxage=900');
+         } else {
+            header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+            header('Pragma: no-cache');
+         }
+      }
+
       header('Content-type: application/json');
-      header($_SERVER['SERVER_PROTOCOL'] . ' ' . $code . ' ' . $this->codes[$code], true, $code);
 
       $response = [
-         'success' => $code < 300,
+         'status' => $code < 300,
          'message' => $message,
+         'data' => $data,
+         'error' => $error,
+         'meta' => $meta
       ];
-
-      if (!is_null($data)) {
-         $response['data'] = $data;
-      } elseif (!is_null($errors)) {
-         $response['errors'] = $errors;
-      }
-
-      if (!empty($meta)) {
-         $response['meta'] = $meta;
-      }
 
       print(json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
    }
