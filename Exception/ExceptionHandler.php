57064315d4e9ce126b6e1c599d48ef3d93f6bc54
cbkmutlu <cbkmutlu@gmail.com>
2025-05-01 01:16:09 +0300

    exception fix

--- Exception/ExceptionHandler.php
+++ Exception/ExceptionHandler.php
@@ -7,25 +7,25 @@ namespace System\Exception;
 use Exception;
 
 class ExceptionHandler extends Exception {
-   public function __construct(string $body, int $code = 417) {
+   public function __construct(string $message, int $code = 500, bool $json = false) {
       $content = isset($_SERVER['CONTENT_TYPE']) && str_contains($_SERVER['CONTENT_TYPE'], 'application/json');
       $accept = isset($_SERVER['HTTP_ACCEPT']) && str_contains($_SERVER['HTTP_ACCEPT'], 'application/json');
 
-      if ($content || $accept) {
+      if (($content || $accept) && $json) {
          header_remove();
          http_response_code($code);
-         header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+
+         header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+         header('Pragma: no-cache');
          header('Content-type: application/json');
 
          print(json_encode([
             'status' => false,
-            'code' => $code,
-            'message' => $body,
-            'data' => null
+            'message' => $message
          ]));
          exit();
       }
 
-      parent::__construct(strip_tags($body), $code);
+      parent::__construct(strip_tags($message), $code);
    }
 }
